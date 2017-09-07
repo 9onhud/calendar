8035605adfc9cda2af45e60c191a6d820fa43a2f
Techasit Anukrua <techasit.a@ku.th>
2017-09-08 00:15:48 +0700

    lab4

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -28,7 +28,6 @@ public class App extends Application{
 			view = loader.getController();
 			view.setController(controller);
 
-
 			Scene scene = new Scene(mainLayout);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Schedule");
